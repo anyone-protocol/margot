e1f4ce80f04034e7a6a10706c9fec88483a6f630
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-10-24 16:46:59 +0300

    ANON-203 - Fix host

--- script.sh
+++ script.sh
@@ -3,11 +3,7 @@
 # Step 1: Copy approved-routers to the Anon data directory (force replacement)
 cp -f approved-routers /usr/src/app/anon-data/approved-routers
 
-printenv
-
-echo $DA_HOST
-
 # Step 2: Authenticate and send reload signal to ControlPort
-echo -e "authenticate \"\"\nsignal reload\nQUIT\n" | nc -v 127.0.0.1 9051
+echo -e "authenticate \"\"\nsignal reload\nQUIT\n" | nc -v $DA_HOST 9051
 
 echo "Configuration reloaded and approved-routers file updated."
