2943ff0d7bf2a0bdcb4a8dcec94eeb0fa724c9bc
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-10-24 16:57:35 +0300

    ANON-203 - Print host

--- script.sh
+++ script.sh
@@ -3,6 +3,8 @@
 # Step 1: Copy approved-routers to the Anon data directory (force replacement)
 cp -f approved-routers /usr/src/app/anon-data/approved-routers
 
+echo $DA_HOST
+
 # Step 2: Authenticate and send reload signal to ControlPort
 echo -e "authenticate \"\"\nsignal reload\nQUIT\n" | nc -v $DA_HOST 9051
 
