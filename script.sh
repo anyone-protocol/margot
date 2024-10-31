f5908cf8d1c0734c6c1463d3244d28f652c9d86c
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-10-31 15:49:21 +0200

    ANON-203 - Change AUTH command

--- script.sh
+++ script.sh
@@ -6,6 +6,9 @@ cp -f approved-routers /usr/src/app/anon-data/approved-routers
 echo $DA_HOST
 
 # Step 2: Authenticate and send reload signal to ControlPort
-echo -e "authenticate \"\"\nsignal reload\nQUIT\n" | nc -v $DA_HOST 9051
+
+COOKIE=$(hexdump -v -e '/1 "%02x"' /usr/src/app/anon-data/control_auth_cookie | tr -d '\n')
+
+echo -e "authenticate ${COOKIE}\nsignal reload\nQUIT\n" | nc 127.0.0.1 9051
 
 echo "Configuration reloaded and approved-routers file updated."
